d9e9457106379a7c4a3d6acc0a48fc05c861f4c6
Muhammad Agung Permana <149060510+1267363@users.noreply.github.com>
2026-03-15 14:55:54 +0700

    Add files via upload

---
+++ src/data.js
@@ -0,0 +1,163 @@
+import HeroImage from "/assets/hero-img.webp";
+
+const Image = {
+  HeroImage,
+};
+
+export default Image;
+
+import Tools1 from "/assets/tools/vscode.png";
+import Tools6 from "/assets/tools/js.png";
+import Tools8 from "/assets/tools/github.png";
+import Tools10 from "/assets/tools/canva.png";
+import Tools14 from "/assets/tools/html.png";
+import Tools15 from "/assets/tools/css.png";
+import Tools00 from "/assets/tools/vercel.ico";
+import Tools111 from "/assets/tools/1.png";
+import Tools222 from "/assets/tools/2.png";
+import Tools333 from "/assets/tools/3.png";
+import Tools444 from "/assets/tools/4.png";
+export const listTools = [
+  {
+    id: 1,
+    gambar: Tools1,
+    nama: "Visual Studio Code",
+    ket: "Code Editor",
+    dad: "100",
+  },
+  {
+    id: 6,
+    gambar: Tools6,
+    nama: "Javascript",
+    ket: "Language",
+    dad: "600",
+  },
+  {
+    id: 8,
+    gambar: Tools8,
+    nama: "Github",
+    ket: "Repository",
+    dad: "800",
+  },
+  {
+    id: 10,
+    gambar: Tools10,
+    nama: "Canva",
+    ket: "Design App",
+    dad: "1000",
+  },
+  {
+    id: 14,
+    gambar: Tools14,
+    nama: "HTML",
+    ket: "Language",
+    dad: "1400",
+  },
+  {
+    id: 15,
+    gambar: Tools15,
+    nama: "CSS",
+    ket: "Language",
+    dad: "1500",
+  },
+  {
+    id: 15,
+    gambar: Tools00,
+    nama: "Vercel",
+    ket: "Deploy",
+    dad: "1500",
+  },
+    {
+    id: 15,
+    gambar: Tools111,
+    nama: "Netflix",
+    ket: "Live streaming",
+    dad: "1500",
+  },
+    {
+    id: 15,
+    gambar: Tools222,
+    nama: "kamera",
+    ket: "Photography",
+    dad: "1500",
+  },
+    {
+    id: 15,
+    gambar: Tools333,
+    nama: "spotify",
+    ket: "Music Streaming",
+    dad: "1500",
+  },
+    {
+    id: 15,
+    gambar: Tools444,
+    nama: "Lightroom",
+    ket: "Photo Editing",
+    dad: "1500",
+  },
+
+];
+
+import Proyek1 from "/assets/proyek/1.png";
+import Proyek2 from "/assets/proyek/2.png";
+import Proyek3 from "/assets/proyek/3.png";
+import Proyek4 from "/assets/proyek/4.png";
+import Proyek5 from "/assets/proyek/5.png";
+import Proyek6 from "/assets/proyek/6.png";
+
+export const listProyek = [
+  {
+    id: 1,
+    image: Proyek1,
+    title: "Website Class",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://class-six-v2.vercel.app/",
+    dad: "100",
+  },
+  {
+    id: 2,
+    image: Proyek2,
+    title: "Website Flower",
+    borderColor: "#10B981",
+    gradient: "linear-gradient(180deg, #10B981, #000)",
+    url: "https://hadiah-sepesial.vercel.app/",
+    dad: "200",
+  },
+  {
+    id: 3,
+    image: Proyek3,
+    title: "Website Class",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://jurumiyah-a.vercel.app/",
+    dad: "300",
+  },
+  {
+    id: 3,
+    image: Proyek4,
+    title: "Website greeting card",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://kartulebaran.vercel.app/",
+    dad: "400",
+  },
+  {
+    id: 3,
+    image: Proyek5,
+    title: "Website Check Khodam",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://kodamku.vercel.app/",
+    dad: "500",
+  },
+   {
+    id: 3,
+    image: Proyek6,
+    title: "Website personal",
+    borderColor: "#3B82F6",
+    gradient: "linear-gradient(145deg, #3B82F6, #000)",
+    url: "https://site-agung.vercel.app/",
+    dad: "600",
+  },
+];
